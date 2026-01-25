2c7f0f30e9ad1d8949ca0d91036e472812f530f8
sDamianos <143997024+sDamianos@users.noreply.github.com>
2026-01-25 14:38:41 +0000

    Add device assignment function for MPI ranks

--- mulitphase Solver/mixedPrecision/main.cu
+++ mulitphase Solver/mixedPrecision/main.cu
@@ -1,4 +1,19 @@
 #include "strdata.h"
+void assign_device_to_rank() {
+
+	int global_rank, local_rank, num_devices;
+	MPI_Comm node_comm;
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &global_rank);
+	MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, global_rank, MPI_INFO_NULL, &node_comm);
+
+	MPI_Comm_rank(node_comm, &local_rank);
+
+	cudaGetDeviceCount(&num_devices);
+	cudaSetDevice(local_rank % num_devices);
+
+	MPI_Comm_free(&node_comm);
+}
 
 int main(int argc, char** argv){
     
@@ -8,7 +23,9 @@ int main(int argc, char** argv){
 	COMM   *comm;
 	
 	MPI_Init(&argc, &argv);
-	
+
+	assign_device_to_rank();
+
 	mesh   = (MESH*)   malloc(sizeof(MESH));
 	field  = (FIELD*)  malloc(sizeof(FIELD));
 	exp    = (EXPORT*) malloc(sizeof(EXPORT));
